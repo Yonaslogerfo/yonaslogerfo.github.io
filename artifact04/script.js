561d40bfa7fbb6e84ed8a7469f52d85d2332ce8c
yonaslogerfo <49923655+Yonaslogerfo@users.noreply.github.com>
2019-06-02 21:40:13 -0700

    Update script.js

--- artifact04/script.js
+++ artifact04/script.js
@@ -7,6 +7,20 @@ function validateForm()
   if (document.getElementById('FirstName').value == "" ){
     txt +=",your first name is missing";
     var txt="firstname not found";
+    //1) Create variable
+var validFirstname=false;
+//2) read value from HTML
+var firstname = document.getElementById("firstname").value;
+//3) Do validation
+if (firstname==="null" || firstname==="" || firstname.length > 20)
+    errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
+else
+   validFirstname = true;
+//4) Send error message to HTML
+document.getElementById("errorMessages").innerHTML = errorMessages;
+
+//5) return status of each field
+return (validFirstname);
   }
  
 //  function show_error()
