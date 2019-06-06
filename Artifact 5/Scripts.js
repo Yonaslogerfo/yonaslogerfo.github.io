a575cb3e9c9d0bedf2ce9e8ac9a77cf4809ad01e
yonaslogerfo <49923655+Yonaslogerfo@users.noreply.github.com>
2019-06-06 15:56:28 -0700

    Update Scripts.js

--- Artifact 5/Scripts.js
+++ Artifact 5/Scripts.js
@@ -1,48 +1,43 @@
-function ValidateForm(){
-  var validUsername = false;
-  var validUserpassword = false;
-  var validFirstName = false;
-  var validPhone = false;
-  var letters = /^[A-Za-z]+$/;
-  var numbers = /^[0-9]+$/;
-  var errorMessages =""; //All the error Messages are going to stay in this variable
-  /***********VALIDATES USERNAEM ******** */
-  //Required filed
-  //This syntax is using name-of-form.name-of-filed.value
-  //you can also use document .getElementById("id-of-filed").value
-  /***********VALIDATES USERNAEM ******** */
-  //Required. Maximum 12 characters.
-  if (myContact.username.value.length > 12 ||
-      myContact.username.value===null ||
-      myContact.username.value==="")
-    errorMessages += "<p>The username must be less than 12 characters and is required</P>";
-  else
-    validUsername =true;
-  //console.log(validUsername);
-  /***********VALIDATES PASSWORD ******** */
-  if (myContact.password.value==null ||
-      myContact.password.value=== "" ||
-      myContact.password.value.length > 7){
-    errorMessages += "<p>The password must be less than 7 characters and it is required</P>";}
-  else
-    validUserPassword =true;
-  /***********VALIDATES FIRSTNAME ******** */
-  if (myContact.firstname.value==null ||
-      myContact.firstname.value=== "" ||
-      myContact.firstname.value.length >20 ||
-      !myContact.firstname.value.match(letters))
-    errorMessages += "<p>The firstname must be less than 20 characters and it is required. only letters and numbers are accepted</P>";
-  else
-    validFirstName =true;
-  /***********VALIDATES PHONE NUMBER ******** */
-  if (myContact.phone.value==null ||
-      myContact.phone.value=== "" ||
-      myContact.phone.value.length >15 ||
-      !myContact.Phone.value.match(numbers))
-    errorMessages += "<p>The phone number must be less than 15 characters and it is required. only numbers are accepted</P>";
-  else
-    validPhone =true;
-  document.getElementById("errorMessages").innerHTML = errorMessages;
-  //make sure you return all the boolean variable that are checking each filed
-  return (validUsername && validUserpassword && validFirstName && validPhone) ;
-}
+var myImages = [" https://www.smartertravel.com/uploads/2017/08/Grand-Canyon-%E2%80%93-hero-1400x500.jpg", 
+               " https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,g_north_west,h_339,q_75,w_610/v1/clients/roanoke/VBR_Sunset_68aa8771-78aa-4cab-a4e6-2035319710d1.jpg", 
+               " https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/vibrant-chicago-skyline-sunset-jasmin-omerovic.jpg", 
+               " https://media.cntraveler.com/photos/5894b2d829a6a9db132b077c/master/pass/GettyImages-118998690.jpg",
+               " https://www.exploregeorgia.org/sites/default/files/styles/slideshow_large/public/legacy_images/forsyth-park-fountain-1-1512424285.jpg?itok=6qo2dtMH"]; 
+var captionImages = 
+["The Grand Canyon!","The Blue Ridge Parkway!", "Chicago’s Skyline!", "Haleakala National Park!","Savannah, Georgia!"]; 
+
+ var index=0; 
+ function updateImage(){ 
+ document.getElementById("slideshow").src = myImages[index]; 
+ document.getElementById("slideshow").alt= captionImages[index]; 
+ document.getElementById("caption").textContent = captionImages[index]; 
+} 
+
+function next(){ 
+ if (myImages.length == index+1) 
+ index=0; 
+ else 
+ index++; 
+ updateImage(); 
+} 
+
+
+function back(){ 
+ if (index===0) 
+ index=myImages.length-1; 
+ else 
+ index--; 
+
+ updateImage(); 
+} 
+
+var nextButton = document.getElementById("next"); 
+var previousButton = document.getElementById("previous"); 
+
+previousButton.addEventListener("click",back,false); 
+nextButton.addEventListener("click",next,false); 
+function autoSlide(){ 
+if (document.getElementById("auto").checked) 
+ next(); 
+} 
+setInterval(autoSlide,3000); // Next 
