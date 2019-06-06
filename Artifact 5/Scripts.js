48b10f2abebd092358a456519d896a4f10b5dd72
yonaslogerfo <49923655+Yonaslogerfo@users.noreply.github.com>
2019-06-06 16:11:44 -0700

    Update Scripts.js

--- Artifact 5/Scripts.js
+++ Artifact 5/Scripts.js
@@ -1,8 +1,8 @@
-var myImages = [" https://www.smartertravel.com/uploads/2017/08/Grand-Canyon-%E2%80%93-hero-1400x500.jpg", 
-               " https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,g_north_west,h_339,q_75,w_610/v1/clients/roanoke/VBR_Sunset_68aa8771-78aa-4cab-a4e6-2035319710d1.jpg", 
-               " https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/vibrant-chicago-skyline-sunset-jasmin-omerovic.jpg", 
-               " https://media.cntraveler.com/photos/5894b2d829a6a9db132b077c/master/pass/GettyImages-118998690.jpg",
-               " https://www.exploregeorgia.org/sites/default/files/styles/slideshow_large/public/legacy_images/forsyth-park-fountain-1-1512424285.jpg?itok=6qo2dtMH"]; 
+var myImages = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-JQ2r9bXui9n-2Tts0de-C2IhNzK6VgwHbKGzUTatAjfsiaby ", 
+               " https://www.ufaw.org.uk/images/about-ufaw/ufaw-strategy-page-main-photo.jpg", 
+               "https://i.kinja-img.com/gawker-media/image/upload/s--O_maD0YQ--/c_scale,f_auto,fl_progressive,q_80,w_800/kdrf3krypm6wk5hbhzme.jpg ", 
+               " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnE48hQJjJjLh3bblezF7013m954KJm_NeZ87cj1QTu577HUYC",
+               " https://images.theconversation.com/files/217599/original/file-20180503-153895-mwmz4a.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=496&fit=clip"]; 
 var captionImages = 
 ["The Grand Canyon!","The Blue Ridge Parkway!", "Chicago’s Skyline!", "Haleakala National Park!","Savannah, Georgia!"]; 
 
